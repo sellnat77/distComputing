1885c19a08b4ebcfbd1e863014a4d5d765a25363
Russell Tan <sell_nat@yahoo.com>
2015-10-05 12:17:11 -0700

    Added server project

---
+++ pythonServer.py
@@ -0,0 +1,24 @@
+from socket import *
+
+tcpIP = '127.0.0.1'
+serverPort = 4242
+serverSocket = socket(AF_INET,SOCK_STREAM)
+serverSocket.bind((tcpIP,serverPort))
+serverSocket.listen(1)
+
+print ('The server is ready to receive')
+run = True
+while run:
+	connectionSocket, addr = serverSocket.accept()
+	sentence = connectionSocket.recv(1024)
+	filename = sentence.split()[1]
+	try:
+		f = open(filename[1:])
+		outputdata = f.read()
+		connectionSocket.send('HTTP/1.0 200 OK\r\nContent-type:text/html;charset=utf8\r\n\r\n')
+		connectionSocket.send(outputdata)
+		connectionSocket.close()
+	except:
+		connectionSocket.send('404 NOT FOUND')
+		connectionSocket.close()
+	
